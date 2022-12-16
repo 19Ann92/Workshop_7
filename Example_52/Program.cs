8e185423374e545792861dd21c686399461674c8
Anna Eliseeva <Panterka6868@mail.ru>
2022-12-16 13:28:38 +0300

    Example 52

---
+++ Example_52/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите 
+// среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк двумерного массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов двумерного массива: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = FillArray(rows, colums);
+string resalt = ArithmeticMeanOfColumn(array);
+
+Console.WriteLine();
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Среднее арифметическое каждого столбца: {resalt}");
+
+int[,] FillArray(int arrayRows, int arrayColumns)
+{
+    int[,] filledArray = new int[arrayRows, arrayColumns];
+    Random random = new Random();
+
+    for (int i = 0; i < arrayRows; i++)
+    {
+        for (int j = 0; j < arrayColumns; j++)
+        {
+            filledArray[i, j] = random.Next(1, 10);
+        }
+    }
+    return filledArray;
+}
+
+void PrintArray(int[,] inputArray)
+{
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write(inputArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+string ArithmeticMeanOfColumn(int[,] array)
+{
+    string b = String.Empty;
+    double resalt = 1;
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            resalt *= array[i,j];
+        }
+        resalt /= array.GetLength(0);
+        b = b + Math.Round(resalt, 2).ToString() + "; ";
+        resalt = 1;
+    }
+    return b;
+}
